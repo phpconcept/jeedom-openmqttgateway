8d3b8d01c16e59ddc0652f9880bfc6e3968a07b4
Vincent <vincent@phpconcept.net>
2025-01-25 15:32:42 +0100

    Amélioration affichage lorsque utilisation du bouton back dans la page html

--- desktop/php/openmqttgateway.php
+++ desktop/php/openmqttgateway.php
@@ -79,7 +79,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			</span>
 		</div>
   <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay" onClick="refreshDeviceList();"><i class="fa fa-arrow-circle-left"></i></a></li>
     <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
     <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
   </ul>
